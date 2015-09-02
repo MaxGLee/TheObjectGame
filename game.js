05a4c1a2d16ab8e2a06bf0fc21cc59e4aa0cb78c
Mike Vickery <mvickery@trugrocer.com>
2015-09-01 19:26:05 -0600

    added player name and hits

--- game.js
+++ game.js
@@ -1,22 +1,30 @@
 var health = 100;
+var name = "Joe";
+var hits = 0;
 
 function slap() {
     health -= 1;
+    hits += 1;
     update();
 };
 
 function punch() {
     health -= 5;
+    hits += 1;
     update();
 };  
 
 function kick() {
     health -= 10;
+    hits += 1;
     update();
 };
 
 function update() {
     var healthStat = document.getElementById("healthStat")
+    var playerName = document.getElementById("playerName")
+    var playerHits = document.getElementById("playerHits")
     healthStat.innerText = health;
-
+    playerName.innerText = name;
+    playerHits.innerText = hits;
 };
