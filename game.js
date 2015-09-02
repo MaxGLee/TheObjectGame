e3dae60ff7d9b93dbc00aa547a5d472c0880c5a4
Mike Vickery <mvickery@trugrocer.com>
2015-09-01 21:21:55 -0600

    player object added

--- game.js
+++ game.js
@@ -1,38 +1,44 @@
-var health = 100;
-var name = "Joe";
-var hits = 0;
+//var Name = "Joe";
+//var health = 100;
+//var hit = 0;
 
-function reset() {
-    health = 100;
-    name = "Joe";
-    hits = 0;
-};
-
-function slap() {
-    health -= 1;
-    hits += 1;
-    update();
-};
-
-function punch() {
-    health -= 5;
-    hits += 1;
-    update();
-};  
-
-function kick() {
-    health -= 10;
-    hits += 1;
-    update();
+var player =
+    {
+        name: "Joe",
+        initialHealth: 100,
+        health: 100,
+        initialHitCount: 0,
+        hitCount: 0,
+        reset: function () {
+            this.name = "Joe";
+            this.health = this.initialHealth;
+            this.hitCount = this.initialHitCount;
+            update();
+        },
+        slap: function () {
+            this.health -= 1;
+            this.hitCount += 1;
+            update();
+        },
+        punch: function () {
+            this.health -= 5;
+            this.hitCount += 1;
+            update();
+        },
+        kick: function () {
+            this.health -= 10;
+            this.hitCount += 1;
+            update();
+        }
 };
 
 function update() {
     var healthStat = document.getElementById("healthStat")
     var playerName = document.getElementById("playerName")
     var playerHits = document.getElementById("playerHits")
-    healthStat.innerText = health;
-    playerName.innerText = name;
-    playerHits.innerText = hits;
+    healthStat.innerText = player.health;
+    playerName.innerText = player.name;
+    playerHits.innerText = player.hitCount;
 };
 
 update();
